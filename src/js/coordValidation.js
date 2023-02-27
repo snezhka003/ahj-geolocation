053ef31fd05e3a27879e4a22fef45721d8203e13
snezhka003 <you@example.com>
2023-02-27 23:30:11 +0300

    оптимизация кода валидации

--- src/js/coordValidation.js
+++ src/js/coordValidation.js
@@ -1,12 +1,13 @@
 export default function coordValidation(input) {
   let data = input;
+  const regularForCoords = /^[-—–−-]?[0-9]{1,2}\.[0-9]+, [-—–−-]?[0-9]{1,2}\.[0-9]+/;
 
   if (data.includes('[') && data.includes(']')) {
     data = data.slice(data.indexOf('[') + 1, data.indexOf(']'));
   }
 
   if (data.includes(' ')) {
-    if (/^[-—–−-]?[0-9]{1,2}\.[0-9]+, [-—–−-]?[0-9]{1,2}\.[0-9]+/.test(data)) {
+    if (regularForCoords.test(data)) {
       return data;
     }
     return false;
@@ -16,7 +17,7 @@ export default function coordValidation(input) {
   const longitude = data.slice(data.indexOf(',') + 1);
   data = `${latitude}, ${longitude}`;
 
-  if (/^[-—–−-]?[0-9]{1,2}\.[0-9]+, [-—–−-]?[0-9]{1,2}\.[0-9]+/.test(data)) {
+  if (regularForCoords.test(data)) {
     return data;
   }
   return false;
